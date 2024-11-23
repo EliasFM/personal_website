cc4642c713235008968b9703fbe56eb1e811197e
Elias Mendel <EliasFM@users.noreply.github.com>
2024-11-23 13:03:15 -0800

    ADD nojekyll and basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/personal-website",
 };
 
 export default nextConfig;
